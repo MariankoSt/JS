78f20e2bc0dd60652ceff56305cb4178b4c4f52f
MarianStepaniukExt <87080132+MarianStepaniukExt@users.noreply.github.com>
2022-02-01 19:11:48 +0200

    Lesson_1_home_work

---
+++ Lesson-1.js
@@ -0,0 +1,53 @@
+/*let hello = "hello";
+console.log(hello);
+document.write(hello);
+hello = "ronarostyslav";
+document.write(hello);
+// first part - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+let firstName = "Test";
+let middleName = "de";
+let lastName = "User";
+let person = firstName+' '+middleName+' '+lastName;
+console.log(person);
+
+// second part -  За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+let firstName = prompt("Your First Name");
+let lastName = prompt("Your Last Name:");
+let age = prompt("Your Age:");
+console.log("Hello "+firstName+" "+lastName+", "+"You are: "+age);
+
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+let a = 100;
+let b = "100";
+let c = 5<6;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
+//  5 ? 6 -> true
+//   5 ? 6 -> false
+//   5 ? 6 -> false
+//   5 ? 6 -> false
+let a = 5 < 6;
+console.log(a);
+let b = 5 > 6;
+console.log(b);
+let c = 5==6;
+console.log(c);
+let d = 5>=6;
+console.log(d);
+*/
+//- Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+//      let a = 5;
+//      document.write(str + a + "<br/>");
+//      document.write(str - a + "<br/>");
+//      document.write(str * "2" + "<br/>");
+//      document.write(str / 2 + "<br/>");
+let str = "20";
+let a = 5;
+document.write(str / 2 + "<br/>");
