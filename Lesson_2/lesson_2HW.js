f03f7083bd4b906f572bcbb788396951cd1095df
MarianStepaniukExt <87080132+MarianStepaniukExt@users.noreply.github.com>
2022-02-04 21:19:25 +0200

    Lesson_2HW - second task finished

--- Lesson_2/lesson_2HW.js
+++ Lesson_2/lesson_2HW.js
@@ -11,3 +11,42 @@ if (time >= 0 && time <= 15) {
 } else {
     console.log('Your clock is broken')
 }
+
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+/*
+// Incorrect solution. Ask somebody why it doesn't work
+const day = 1;
+
+switch (day) {
+    case day >= 1 && day <= 10 :
+        console.log(`Selected day ${day} is in the first decade of the month`);
+        break;
+    case day >= 11 && day <= 20 :
+        console.log(`Selected day ${day} is in the second decade of the month`);
+        break;
+    case day >= 21 && day <= 31 :
+        console.log(`Selected day ${day} is in the third decade of the month`);
+        break;
+    default :
+        console.log(`You have entered an invalid day of the month. Your input is ${day}. Please select a number from 1 to 31.`);
+        break;
+}
+*/
+
+
+
+/*
+
+// correct solution
+const day = 31;
+if (day >= 1 && day <= 10) {
+    console.log(`Selected day ${day} is in the first decade of the month.`);
+} else if (day >= 11 && day <= 20) {
+    console.log(`Selected day ${day} is in the second decade of the month.`);
+} else if (day >= 21 && day <= 31) {
+    console.log(`Selected day ${day} is in the third decade of the month.`);
+} else {
+    console.log(`You have entered an invalid day of the month. Your input is ${day}. Please select a number from 1 to 31.`);
+}
+
+*/
